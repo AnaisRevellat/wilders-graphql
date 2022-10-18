0d96f323f1bcf617921f4d01a0e1dacf67813c02
AnaisRevellat <anaisrevellat@gmail.com>
2022-10-18 15:06:02 +0200

    server and updates

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,48 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import datasource from "./utils";
-import wildersController from "./controllers/Wilders"; // → objet (key-value)
-import skillsController from "./controllers/Skills";
-import upvotesController from "./controllers/Upvotes";
 
-const app = express();
+const typeDefs = gql`
+	type Wilder {
+		id: ID
+		name: String
+		#upvotes
+	}
 
-app.use(cors());
-app.use(express.json());
+	type Query {
+		wilders: [Wilder]
+	}
+`;
 
-app.get("/", (req, res) => {
-  res.send("Hello you!");
-});
-
-// High order function = function that returns a function
-type Controller = (req: Request, res: Response) => void;
-const asyncHandler = (controller: Controller): Controller => {
-  return async function (req: Request, res: Response): Promise<void> {
-    try {
-      await controller(req, res);
-    } catch (err: any) {
-      console.error("Error: ", err);
-      res.status(500).json({ error: err.message || "Error occured" });
-    }
-  };
+const resolvers = {
+	Query: {
+		wilders: () => [],
+	},
 };
 
-// Pur function, same input = same output
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-/**
- * Wilders Routes
- */
-app.post("/api/wilders", wildersController.create);
-app.get("/api/wilders", wildersController.findAll);
-app.get("/api/wilders/:wilderId", asyncHandler(wildersController.find));
-app.put("/api/wilders/:wilderId", asyncHandler(wildersController.update));
-app.delete("/api/wilders/:wilderId", wildersController.delete);
-
-/**
- * Skills Routes
- */
-app.post("/api/skills", skillsController.create);
-app.get("/api/skills", skillsController.findAll);
-
-/**
- * Upvotes Routes
- */
-app.post("/api/upvotes", asyncHandler(upvotesController.create));
-app.put(
-  "/api/upvotes/:upvoteId/upvote",
-  asyncHandler(upvotesController.upvote)
-);
-
-// end of request
-
-app.listen(5000, async () => {
-  console.log("Server started, youpi!");
-
-  /**
-   * datasource.initialize()
-   *  .then(() => console.log("I'm connected!"))
-   *  .catch(() => console.log("Dommage"))
-   */
-
-  try {
-    await datasource.initialize();
-    console.log("I'm connected!");
-  } catch (err) {
-    console.log("Dommage");
-    console.error(err);
-  }
+// server
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	csrfPrevention: true,
+	cache: "bounded",
+	plugins: [
+		ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+	],
 });
 
-// 2 bonuses
-// → add upvotes to wilder skill
-// → uploaded un avatar, package → multer
+// The `listen` method launches a web server.
+server.listen(5000).then(
+	async () => {
+		console.log(`🚀  Server sarted `);
+
+		try {
+			await datasource.initialize();
+			console.log("I'm connected!");
+		} catch (err) {
+			console.log("Dommage");
+			console.error(err);
+		}
+	},
+	(err) => console.error(err)
+);
